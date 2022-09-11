d749fdec687b12cc92a2952217e8d81034df250e
Raghuvendhiran <92200441+Raghuvendhiran@users.noreply.github.com>
2022-09-11 22:43:06 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BootCard from './/Tasks//1sttask-Card//index';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BootCard />
+  </React.StrictMode>
+);
+
+
